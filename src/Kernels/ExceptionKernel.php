e942f28d93b590f9c07ae5f0455a781e46ca86f6
Anthony Vancauwenberghe <anthony@officeline.be>
2019-06-29 15:01:50 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Kernels/ExceptionKernel.php
+++ src/Kernels/ExceptionKernel.php
@@ -13,7 +13,8 @@ class ExceptionKernel extends ExceptionHandler
      * Render an exception into an HTTP response.
      *
      * @param \Illuminate\Http\Request $request
-     * @param \Exception $exception
+     * @param \Exception               $exception
+     *
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
@@ -23,28 +24,30 @@ class ExceptionKernel extends ExceptionHandler
             parent::render($request, $exception);
     }
 
-    protected function requestComesFromApiDomain($request){
-        return Str::contains($this->getFormattedRequestUrl($request),$this->getFormattedApiUrl());
+    protected function requestComesFromApiDomain($request)
+    {
+        return Str::contains($this->getFormattedRequestUrl($request), $this->getFormattedApiUrl());
     }
 
     protected function getFormattedApiUrl()
     {
         $larapie = new LarapieManager();
-        return parse_url($larapie->getApiUrl())['host'] . (parse_url($larapie->getApiUrl())['path'] ?? '');
+
+        return parse_url($larapie->getApiUrl())['host'].(parse_url($larapie->getApiUrl())['path'] ?? '');
     }
 
     protected function getFormattedRequestUrl($request)
     {
-        return parse_url($request->url())['host'] . parse_url($request->url())['path'];
+        return parse_url($request->url())['host'].parse_url($request->url())['path'];
     }
 
     protected function jsonErrorResponse(Exception $exception)
     {
         return response()->json([
             'error' => [
-                'message' => $exception->getMessage(),
-                'status_code' => $exception->getStatusCode()
-            ]
+                'message'     => $exception->getMessage(),
+                'status_code' => $exception->getStatusCode(),
+            ],
         ])->setStatusCode($exception->getStatusCode());
     }
 }
